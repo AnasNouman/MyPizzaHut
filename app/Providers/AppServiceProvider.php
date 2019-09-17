88fd29a7e331578681d85bc9ae7726b4ddc98d56
AnasNouman <anasnouman14@gmail.com>
2019-09-17 20:37:28 +0500

    scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // force HTTPS
-        URL::forceSchema('https');
+        URL::forceScheme('https');
     }
 
     /**
