7ab31db50d5c5a8eab935ddef180baa4f2ebf782
AnasNouman <anasnouman14@gmail.com>
2019-09-17 23:33:07 +0500

    added postgres database configuration

--- public/js/app.js
+++ public/js/app.js
@@ -78059,8 +78059,6 @@ function (_Component) {
         price: this.state.price,
         address: this.state.address
       };
-      console.log("OKAY1234");
-      console.log(orders);
       axios__WEBPACK_IMPORTED_MODULE_4___default.a.post('http://127.0.0.1:8000/myorders/store', orders).then(function (res) {
         return Console.log(res.data);
       });
